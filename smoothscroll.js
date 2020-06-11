8e61d5cf9f8469fce97b24bde7ff4446c4f3bedb
Sophia <sophia-smile-ha@hotmail.com>
2020-06-11 02:00:24 -0600

    06-11-2020 2AM
    Layout integrated across page;
    Added writing verticl tabs;
    Links functional;

--- smoothscroll.js
+++ smoothscroll.js
@@ -2,7 +2,6 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">
 </script>
 
-<script>
 $(document).ready(function(){
   // Add smooth scrolling to all links
   $("a").on('click', function(event) {
