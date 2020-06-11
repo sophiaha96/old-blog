be1af37b8a9819f430444299dcf3cd572e521288
Sophia <sophia-smile-ha@hotmail.com>
2020-06-10 22:41:00 -0600

    06-10-2020

---
+++ response.js
@@ -0,0 +1,13 @@
+// Nav Bar always shows
+window.onscroll = function(){
+	scrollNav()
+};
+
+function scrollNav(){
+	if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+		document.getElementById("topNav").style.top = "0";
+	}
+	else {
+		document.getElementById("topNav").style.top = "-80px";
+	}
+}
